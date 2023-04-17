1cfe9e7950b6f9a410cba4327892d23a2fdfdc20
Ivan Rossi <ivanross.me@gmail.com>
2023-04-17 15:16:15 +0200

    add data components doc

--- documentation/src/App.tsx
+++ documentation/src/App.tsx
@@ -1,6 +1,6 @@
 import { CodeSandboxEmbed } from './CodesandboxEmbed'
 import { Code } from './Code'
-import { CodeSandbox } from './CodeSandobx'
+import { CodeSandbox } from './CodeSandbox'
 
 function App() {
   return (
@@ -39,6 +39,48 @@ function App() {
       </h2>
       <ExampleGrid />
 
+      <h2>Data components</h2>
+      <h3>
+        <Code>PointData</Code>
+      </h3>
+      <ExamplePointData />
+
+      <h3>
+        <Code>LineData</Code>
+      </h3>
+      <ExampleLineData />
+
+      <h3>
+        <Code>AreaData</Code>
+      </h3>
+      <ExampleAreaData />
+
+      <h3>
+        <Code>RectData</Code>
+      </h3>
+      <ExampleRectData />
+
+      <h3>
+        <Code>BarData</Code>
+      </h3>
+      <ExampleBarData />
+
+      <h3>
+        <Code>LabelsData</Code>
+      </h3>
+      <ExampleLabelData />
+
+      <h2>Function components</h2>
+      <h3>
+        <Code>LineFunction</Code>
+      </h3>
+      <ExampleLineFunction />
+
+      <h3>
+        <Code>AreaFunction</Code>
+      </h3>
+      <ExampleAreaFunction />
+
       <CodeSandboxEmbed src="https://codesandbox.io/embed/rcc-scatterplot-gus8g?fontsize=14&hidenavigation=1&theme=dark" />
 
       <CodeSandboxEmbed src="https://codesandbox.io/embed/rcc-stacked-area-graph-d1xrx?fontsize=14&hidenavigation=1&theme=dark" />
@@ -48,7 +90,7 @@ function App() {
 
 export default App
 
-const dataTs = /* tsx */ `export const dataset = [
+const dataTs = `export const dataset = [
   { key: "e", x: 69.04, y: 0.04 },
   { key: "d", x: 26.87, y: 0.21 },
   { key: "c", x: 95.28, y: 0.72 },
@@ -66,7 +108,7 @@ const dataTs = /* tsx */ `export const dataset = [
   { key: "m", x: 72.24, y: 0.13 },
 ];
 `
-
+const COLOR = '#6bc2be'
 const ExampleInstall = () => (
   <CodeSandbox
     template="vanilla"
@@ -87,7 +129,7 @@ const ExampleBasicStructure = () => (
     hideTabs
     readOnly
     files={{
-      'App.tsx': /* tsx */ `<svg>
+      'App.tsx': `<svg>
   <Chart>
     <Cartesian>
       <Grid />
@@ -108,7 +150,7 @@ export const ExampleChart = () => (
     editorStyle={{ height: 320 }}
     files={{
       '/App.tsx': {
-        code: /* tsx */ `import { Chart } from 'react-composable-charts'
+        code: `import { Chart } from 'react-composable-charts'
 
 export default function App() {
   const width = 200
@@ -134,7 +176,7 @@ export const ExampleCartesian = () => (
     editorStyle={{ height: 540 }}
     files={{
       '/App.tsx': {
-        code: /* tsx */ `import { Chart, Cartesian } from "react-composable-charts";
+        code: `import { Chart, Cartesian } from "react-composable-charts";
 import * as d3 from "d3";
 import { dataset } from "./data";
 
@@ -169,18 +211,18 @@ export const ExampleGrid = () => (
     preivewStyle={{ height: 540 }}
     files={{
       '/App.tsx': {
-        code: /* tsx */ `import { Chart, Cartesian, Grid } from "react-composable-charts";
+        code: `import { Chart, Cartesian, Grid } from "react-composable-charts";
 import * as d3 from "d3";
 import { dataset } from "./data";
 
 const ChartGrid = () => (
   <Grid>
-    <Grid.XLines stroke="blue" />
-    <Grid.YLines stroke="blue" />
-    <Grid.XLabels stroke="red" padding={5} />
-    <Grid.YLabels stroke="red" padding={5} />
+    <Grid.XLines stroke="grey" />
+    <Grid.YLines stroke="grey" />
     <Grid.XAxes stroke="black" strokeWidth={2} />
     <Grid.YAxes stroke="black" strokeWidth={2} />
+    <Grid.XLabels padding={5} />
+    <Grid.YLabels padding={5} />
   </Grid>
 )
 
@@ -213,6 +255,524 @@ export default function App() {
 `,
       },
       'data.ts': dataTs,
+    }}
+  />
+)
+
+export const ExamplePointData = () => (
+  <CodeSandbox
+    editorStyle={{ height: 540 }}
+    preivewStyle={{ height: 540 }}
+    files={{
+      '/App.tsx': {
+        code: `import { Chart, Cartesian, Grid, PointData } from "react-composable-charts";
+import * as d3 from "d3";
+import { dataset } from "./data";
+
+const Data = () => (
+  <PointData
+    data={dataset}
+    x={(d) => d.x}
+    y={(d) => d.y}
+    r={7}
+    fill="${COLOR}"   
+  />
+);
+
+export default function App() {
+  const width = 400;
+  const height = 400;
+  const padding = 30;
+  const xDomain = d3.extent(dataset, (d) => d.x) as [number, number];
+  const yDomain = d3.extent(dataset, (d) => d.y) as [number, number];
+  return (
+    <svg width={width} height={height}>
+      <Chart
+        top={padding}
+        left={padding}
+        width={width - padding * 2}
+        height={height - padding * 2}
+      >
+        <Cartesian
+          x={{ scale: "linear", domain: xDomain }}
+          y={{ scale: "linear", domain: yDomain }}
+          nice={true}
+        >
+          <ChartGrid />
+          <Data />
+        </Cartesian>
+      </Chart>
+    </svg>
+  );
+}
+
+const ChartGrid = () => (
+  <Grid>
+    <Grid.XLines stroke="grey" />
+    <Grid.YLines stroke="grey" />
+    <Grid.XAxes stroke="black" strokeWidth={2} />
+    <Grid.YAxes stroke="black" strokeWidth={2} />
+    <Grid.XLabels padding={5} />
+    <Grid.YLabels padding={5} />
+  </Grid>
+)
+              
+`,
+      },
+      'data.ts': dataTs,
+    }}
+  />
+)
+
+export const ExampleLineData = () => (
+  <CodeSandbox
+    editorStyle={{ height: 540 }}
+    preivewStyle={{ height: 540 }}
+    files={{
+      '/App.tsx': {
+        code: `import { Chart, Cartesian, Grid, LineData } from "react-composable-charts";
+import * as d3 from "d3";
+import { dataset } from "./data";
+
+const Data = () => (
+  <LineData
+    data={d3.sort(dataset, (d) => d.x)}
+    x={(d) => d.x}
+    y={(d) => d.y}
+    strokeWidth={2}
+    stroke="${COLOR}"
+  />
+);
+
+export default function App() {
+  const width = 400;
+  const height = 400;
+  const padding = 30;
+  const xDomain = d3.extent(dataset, (d) => d.x) as [number, number];
+  const yDomain = d3.extent(dataset, (d) => d.y) as [number, number];
+  return (
+    <svg width={width} height={height}>
+      <Chart
+        top={padding}
+        left={padding}
+        width={width - padding * 2}
+        height={height - padding * 2}
+      >
+        <Cartesian
+          x={{ scale: "linear", domain: xDomain }}
+          y={{ scale: "linear", domain: yDomain }}
+          nice={true}
+        >
+          <ChartGrid />
+          <Data />
+        </Cartesian>
+      </Chart>
+    </svg>
+  );
+}
+
+const ChartGrid = () => (
+  <Grid>
+    <Grid.XLines stroke="grey" />
+    <Grid.YLines stroke="grey" />
+    <Grid.XAxes stroke="black" strokeWidth={2} />
+    <Grid.YAxes stroke="black" strokeWidth={2} />
+    <Grid.XLabels padding={5} />
+    <Grid.YLabels padding={5} />
+  </Grid>
+)
+              
+`,
+      },
+      'data.ts': dataTs,
+    }}
+  />
+)
+
+export const ExampleAreaData = () => (
+  <CodeSandbox
+    editorStyle={{ height: 540 }}
+    preivewStyle={{ height: 540 }}
+    files={{
+      '/App.tsx': {
+        code: `import { Chart, Cartesian, Grid, AreaData } from "react-composable-charts";
+import * as d3 from "d3";
+import { dataset } from "./data";
+
+const Data = () => (
+  <AreaData
+    data={d3.sort(dataset, (d) => d.x)}
+    x={(d) => d.x}
+    y={(d) => d.y}
+    fill="${COLOR}"
+  />
+);
+
+export default function App() {
+  const width = 400;
+  const height = 400;
+  const padding = 30;
+  const xDomain = d3.extent(dataset, (d) => d.x) as [number, number];
+  const yDomain = d3.extent(dataset, (d) => d.y) as [number, number];
+  return (
+    <svg width={width} height={height}>
+      <Chart
+        top={padding}
+        left={padding}
+        width={width - padding * 2}
+        height={height - padding * 2}
+      >
+        <Cartesian
+          x={{ scale: "linear", domain: xDomain }}
+          y={{ scale: "linear", domain: yDomain }}
+          nice={true}
+        >
+          <ChartGrid />
+          <Data />
+        </Cartesian>
+      </Chart>
+    </svg>
+  );
+}
+
+const ChartGrid = () => (
+  <Grid>
+    <Grid.XLines stroke="grey" />
+    <Grid.YLines stroke="grey" />
+    <Grid.XAxes stroke="black" strokeWidth={2} />
+    <Grid.YAxes stroke="black" strokeWidth={2} />
+    <Grid.XLabels padding={5} />
+    <Grid.YLabels padding={5} />
+  </Grid>
+)
+              
+`,
+      },
+      'data.ts': dataTs,
+    }}
+  />
+)
+
+export const ExampleRectData = () => (
+  <CodeSandbox
+    editorStyle={{ height: 540 }}
+    preivewStyle={{ height: 540 }}
+    files={{
+      '/App.tsx': {
+        code: `import { Chart, Cartesian, Grid, RectData } from "react-composable-charts";
+import * as d3 from "d3";
+import { dataset } from "./data";
+
+const Data = () => (
+  <RectData
+    data={dataset}
+    x={(d) => d.x}
+    y={(d) => d.y}
+    width={20}
+    height={20}
+    rx={6}
+    fill="${COLOR}"
+  />
+);
+
+export default function App() {
+  const width = 400;
+  const height = 400;
+  const padding = 30;
+  const xDomain = d3.extent(dataset, (d) => d.x) as [number, number];
+  const yDomain = d3.extent(dataset, (d) => d.y) as [number, number];
+  return (
+    <svg width={width} height={height}>
+      <Chart
+        top={padding}
+        left={padding}
+        width={width - padding * 2}
+        height={height - padding * 2}
+      >
+        <Cartesian
+          x={{ scale: "linear", domain: xDomain }}
+          y={{ scale: "linear", domain: yDomain }}
+          nice={true}
+        >
+          <ChartGrid />
+          <Data />
+        </Cartesian>
+      </Chart>
+    </svg>
+  );
+}
+
+const ChartGrid = () => (
+  <Grid>
+    <Grid.XLines stroke="grey" />
+    <Grid.YLines stroke="grey" />
+    <Grid.XAxes stroke="black" strokeWidth={2} />
+    <Grid.YAxes stroke="black" strokeWidth={2} />
+    <Grid.XLabels padding={5} />
+    <Grid.YLabels padding={5} />
+  </Grid>
+)
+              
+`,
+      },
+      'data.ts': dataTs,
+    }}
+  />
+)
+
+export const ExampleBarData = () => (
+  <CodeSandbox
+    editorStyle={{ height: 540 }}
+    preivewStyle={{ height: 540 }}
+    files={{
+      '/App.tsx': {
+        code: `import { Chart, Cartesian, Grid, BarData } from "react-composable-charts";
+import * as d3 from "d3";
+import { dataset } from "./data";
+
+const Data = () => (
+  <BarData
+    data={dataset}
+    x={(d) => d.key}
+    y={{ to: (d) => d.y }}
+    fill="${COLOR}"
+  />
+);
+
+export default function App() {
+  const width = 400;
+  const height = 400;
+  const padding = 30;
+  const xDomain = dataset.map((d) => d.key);
+  const yDomain = d3.extent(dataset, (d) => d.y) as [number, number];
+  return (
+    <svg width={width} height={height}>
+      <Chart
+        top={padding}
+        left={padding}
+        width={width - padding * 2}
+        height={height - padding * 2}
+      >
+        <Cartesian
+          x={{ scale: "band", domain: xDomain }}
+          y={{ scale: "linear", domain: yDomain }}
+          nice={true}
+        >
+          <ChartGrid />
+          <Data />
+        </Cartesian>
+      </Chart>
+    </svg>
+  );
+}
+
+const ChartGrid = () => (
+  <Grid>
+    <Grid.XLines stroke="grey" />
+    <Grid.YLines stroke="grey" />
+    <Grid.XAxes stroke="black" strokeWidth={2} />
+    <Grid.YAxes stroke="black" strokeWidth={2} />
+    <Grid.XLabels padding={5} />
+    <Grid.YLabels padding={5} />
+  </Grid>
+)
+              
+`,
+      },
+      'data.ts': dataTs,
+    }}
+  />
+)
+
+export const ExampleLabelData = () => (
+  <CodeSandbox
+    editorStyle={{ height: 540 }}
+    preivewStyle={{ height: 540 }}
+    files={{
+      '/App.tsx': {
+        code: `import { Chart, Cartesian, Grid, PointData, LabelsData } from "react-composable-charts";
+import * as d3 from "d3";
+import { dataset } from "./data";
+
+const Data = () => (
+  <>
+    <LabelsData
+      data={dataset}
+      dataX={(d) => d.x}
+      dataY={(d) => d.y}
+      textAnchor="middle"
+      offsetY={-10}
+      text={(d) => d.key}
+    />
+    <PointData
+      data={dataset}
+      x={(d) => d.x}
+      y={(d) => d.y}
+      r={2}
+      fill="${COLOR}"   
+    />
+  </>
+);
+
+export default function App() {
+  const width = 400;
+  const height = 400;
+  const padding = 30;
+  const xDomain = d3.extent(dataset, (d) => d.x) as [number, number];
+  const yDomain = d3.extent(dataset, (d) => d.y) as [number, number];
+  return (
+    <svg width={width} height={height}>
+      <Chart
+        top={padding}
+        left={padding}
+        width={width - padding * 2}
+        height={height - padding * 2}
+      >
+        <Cartesian
+          x={{ scale: "linear", domain: xDomain }}
+          y={{ scale: "linear", domain: yDomain }}
+          nice={true}
+        >
+          <ChartGrid />
+          <Data />
+        </Cartesian>
+      </Chart>
+    </svg>
+  );
+}
+
+const ChartGrid = () => (
+  <Grid>
+    <Grid.XLines stroke="grey" />
+    <Grid.YLines stroke="grey" />
+    <Grid.XAxes stroke="black" strokeWidth={2} />
+    <Grid.YAxes stroke="black" strokeWidth={2} />
+    <Grid.XLabels padding={5} />
+    <Grid.YLabels padding={5} />
+  </Grid>
+)
+              
+`,
+      },
+      'data.ts': dataTs,
+    }}
+  />
+)
+
+export const ExampleLineFunction = () => (
+  <CodeSandbox
+    editorStyle={{ height: 540 }}
+    preivewStyle={{ height: 540 }}
+    hideTabs
+    files={{
+      '/App.tsx': {
+        code: `import { Chart, Cartesian, Grid, LineFunction } from "react-composable-charts";
+
+const Data = () => (
+  <LineFunction
+    fn={(x) => Math.sin(x * Math.PI * 2) * 0.5}
+    stroke="${COLOR}"
+    strokeWidth={2}
+  />
+);
+
+export default function App() {
+  const width = 400;
+  const height = 400;
+  const padding = 30;
+  return (
+    <svg width={width} height={height}>
+      <Chart
+        top={padding}
+        left={padding}
+        width={width - padding * 2}
+        height={height - padding * 2}
+      >
+        <Cartesian
+          x={{ scale: "linear", domain: [-1, 1] }}
+          y={{ scale: "linear", domain: [-1, 1] }}
+        >
+          <ChartGrid />
+          <Data />
+        </Cartesian>
+      </Chart>
+    </svg>
+  );
+}
+
+const ChartGrid = () => (
+  <Grid>
+    <Grid.XLines stroke="grey" />
+    <Grid.YLines stroke="grey" />
+    <Grid.XAxes stroke="black" strokeWidth={2} />
+    <Grid.YAxes stroke="black" strokeWidth={2} />
+    <Grid.XLabels padding={5} />
+    <Grid.YLabels padding={5} />
+  </Grid>
+);
+
+              
+`,
+      },
+    }}
+  />
+)
+
+export const ExampleAreaFunction = () => (
+  <CodeSandbox
+    editorStyle={{ height: 540 }}
+    preivewStyle={{ height: 540 }}
+    hideTabs
+    files={{
+      '/App.tsx': {
+        code: `import { Chart, Cartesian, Grid, AreaFunction } from "react-composable-charts";
+
+const Data = () => (
+  <AreaFunction
+    fn={(x) => Math.sin(x * Math.PI * 2) * 0.5 + 0.25}
+    fnBase={(x) => Math.sin(x * Math.PI * 2) * 0.5 - 0.25}
+    fill="${COLOR}"
+    opacity={0.5}
+  />
+);
+
+export default function App() {
+  const width = 400;
+  const height = 400;
+  const padding = 30;
+  return (
+    <svg width={width} height={height}>
+      <Chart
+        top={padding}
+        left={padding}
+        width={width - padding * 2}
+        height={height - padding * 2}
+      >
+        <Cartesian
+          x={{ scale: "linear", domain: [-1, 1] }}
+          y={{ scale: "linear", domain: [-1, 1] }}
+        >
+          <ChartGrid />
+          <Data />
+        </Cartesian>
+      </Chart>
+    </svg>
+  );
+}
+
+const ChartGrid = () => (
+  <Grid>
+    <Grid.XLines stroke="grey" />
+    <Grid.YLines stroke="grey" />
+    <Grid.XAxes stroke="black" strokeWidth={2} />
+    <Grid.YAxes stroke="black" strokeWidth={2} />
+    <Grid.XLabels padding={5} />
+    <Grid.YLabels padding={5} />
+  </Grid>
+);
+              
+`,
+      },
     }}
   />
 )
