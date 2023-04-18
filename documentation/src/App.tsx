92ce46d2a9a0d5720643a1936e40f087a10c1c9f
Ivan Rossi <ivanross.me@gmail.com>
2023-04-18 14:17:11 +0200

    refactor examples, remove sub components

--- documentation/src/App.tsx
+++ documentation/src/App.tsx
@@ -2,6 +2,56 @@ import { CodeSandboxEmbed } from './CodesandboxEmbed'
 import { Code } from './Code'
 import { CodeSandbox } from './CodeSandbox'
 
+function Table({ cells }: { cells: React.ReactNode[][] }) {
+  const tdStyle = {
+    border: '1px solid #cdcdcd',
+    padding: '0.75rem',
+  }
+  return (
+    <table style={{ border: '1px solid #cdcdcd', borderCollapse: 'collapse' }}>
+      <tbody style={{ border: 'inherit' }}>
+        {cells.map((row, i) => (
+          <tr key={i}>
+            {row.map((value, j) => (
+              <td key={j} style={tdStyle}>
+                {value}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+function PropsTable({
+  props,
+}: {
+  props: {
+    name: string
+    doc: React.ReactNode
+    default?: string
+    optional?: boolean
+  }[]
+}) {
+  return (
+    <Table
+      cells={props.map((row) => [
+        <Code>{row.name}</Code>,
+        <>
+          {row.doc}{' '}
+          {row.default && (
+            <>
+              <i>Default</i>: <Code>{row.default}</Code>
+            </>
+          )}{' '}
+          {row.optional && <i> Optional.</i>}
+        </>,
+      ])}
+    />
+  )
+}
+
 function App() {
   return (
     <div className="main">
@@ -27,19 +77,86 @@ function App() {
       <h2>
         <Code>Chart</Code> component
       </h2>
+
+      <p>
+        <Code>Chart</Code> defines where the chart is placed in the svg element.
+        It also defines the size of the chart. The chart is placed in the svg
+        element at the position defined by <Code>left</Code> and{' '}
+        <Code>top</Code> props. The size of the chart is defined by{' '}
+        <Code>width</Code> and <Code>height</Code> props.
+      </p>
       <ExampleChart />
 
       <h2>
         <Code>Cartesian</Code> component
       </h2>
+
+      <p>
+        <Code>Cartesian</Code> create a "cartesian" chart. It defines the scales
+        used to map data to the chart. The scales are defined by the{' '}
+        <Code>x</Code> and <Code>y</Code> prop. It also allows to define an
+        optional <Code>color</Code> scale.
+      </p>
+
+      <p>
+        The component accepts also an optional <Code>nice</Code> prop that
+        allows to use nice scales.
+      </p>
       <ExampleCartesian />
 
       <h2>
         <Code>Grid</Code> component
       </h2>
+
+      <p>
+        <Code>Grid</Code> component allows to add a grid to the chart. It is
+        composed of several sub-components: <Code>XLines</Code>,{' '}
+        <Code>YLines</Code>, <Code>XLabels</Code>, <Code>YLabels</Code>,{' '}
+        <Code>XAxes</Code> and <Code>YAxes</Code>.
+      </p>
+
+      <p>
+        Every sub components can be used and styled independently. For example,
+        if you want to add only the x lines, you can use the <Code>XLines</Code>{' '}
+        component.
+      </p>
+
+      <p>
+        It is also possible to pass props to <Code>Grid</Code> component that
+        reflect to the entire grid, such as <Code>tickCount</Code> or{' '}
+        <Code>tickSize</Code>.
+      </p>
+
       <ExampleGrid />
 
       <h2>Data components</h2>
+
+      <p>
+        Data components are used to render data in the chart. They usually
+        accepts a <Code>data</Code> prop that is an array of objects. Each
+        object represents a data point. The data components also accepts a{' '}
+        <Code>dataKey</Code> prop that is used to uniquely identify each data
+        point. The <Code>dataKey</Code> prop is used to animate the data points.
+      </p>
+
+      <p>
+        Data components also accepts a <Code>x</Code> and <Code>y</Code> prop.
+        These are used to map the data point to the chart. This props should be
+        a function that accepts an element of <Code>data</Code> and returns the{' '}
+        <b>value</b> of the data point that should be passed to the x and y
+        scales.
+      </p>
+
+      <p>
+        Finally, data components accept styiling props such as <Code>fill</Code>
+        , <Code>stroke</Code>, <Code>strokeWidth</Code> etc. If a{' '}
+        <Code>color</Code> prop is passed to the containing{' '}
+        <Code>Cartesian</Code> component, a function can be passed to{' '}
+        <Code>fill</Code> and <Code>stroke</Code> props. This function will be
+        called per element of <Code>data</Code> and should return the{' '}
+        <b>value</b> to be passed to the color scale.
+      </p>
+
       <h3>
         <Code>PointData</Code>
       </h3>
@@ -68,6 +185,30 @@ function App() {
       <h3>
         <Code>LabelsData</Code>
       </h3>
+
+      <p>
+        <Code>LabelsData</Code> is a special data component that allows to
+        render labels for each data points. Instead of accepting only{' '}
+        <Code>x</Code> and <Code>y</Code> props, it accepts:
+        <ul>
+          <li>
+            <Code>dataX</Code> and <Code>dataY</Code> are functions that accept
+            an element of data and returns the <b>value</b> of the data point
+            that should be passed to the x and y scales. These works like the{' '}
+            <Code>x</Code> and <Code>y</Code> props of the other data
+            components.
+          </li>
+          <li>
+            <Code>offsetX</Code> and <Code>offsetY</Code> are number
+            representing the offset in pixels from the position defined by{' '}
+            <Code>dataX</Code> and <Code>dataY</Code>.
+          </li>
+          <li>
+            <Code>positionX</Code> and <Code>positionY</Code> are functions that
+            accept an element of data and returns the <b>position</b> in pixels.
+          </li>
+        </ul>
+      </p>
       <ExampleLabelData />
 
       <h2>Function components</h2>
@@ -215,17 +356,6 @@ export const ExampleGrid = () => (
 import * as d3 from "d3";
 import { dataset } from "./data";
 
-const ChartGrid = () => (
-  <Grid>
-    <Grid.XLines stroke="grey" />
-    <Grid.YLines stroke="grey" />
-    <Grid.XAxes stroke="black" strokeWidth={2} />
-    <Grid.YAxes stroke="black" strokeWidth={2} />
-    <Grid.XLabels padding={5} />
-    <Grid.YLabels padding={5} />
-  </Grid>
-)
-
 export default function App() {
   const width = 400;
   const height = 400;
@@ -245,13 +375,19 @@ export default function App() {
           y={{ scale: "linear", domain: yDomain }}
           nice={true}
         >
-          <ChartGrid />
+          <Grid>
+            <Grid.XLines stroke="grey" />
+            <Grid.YLines stroke="grey" />
+            <Grid.XAxes stroke="black" strokeWidth={2} />
+            <Grid.YAxes stroke="black" strokeWidth={2} />
+            <Grid.XLabels padding={5} />
+            <Grid.YLabels padding={5} />
+          </Grid>
         </Cartesian>
       </Chart>
     </svg>
   );
 }
-              
 `,
       },
       'data.ts': dataTs,
@@ -269,16 +405,6 @@ export const ExamplePointData = () => (
 import * as d3 from "d3";
 import { dataset } from "./data";
 
-const Data = () => (
-  <PointData
-    data={dataset}
-    x={(d) => d.x}
-    y={(d) => d.y}
-    r={7}
-    fill="${COLOR}"   
-  />
-);
-
 export default function App() {
   const width = 400;
   const height = 400;
@@ -296,27 +422,29 @@ export default function App() {
         <Cartesian
           x={{ scale: "linear", domain: xDomain }}
           y={{ scale: "linear", domain: yDomain }}
-          nice={true}
+          nice
         >
-          <ChartGrid />
-          <Data />
+          <Grid>
+            <Grid.XLines stroke="grey" />
+            <Grid.YLines stroke="grey" />
+            <Grid.XAxes stroke="black" strokeWidth={2} />
+            <Grid.YAxes stroke="black" strokeWidth={2} />
+            <Grid.XLabels padding={5} />
+            <Grid.YLabels padding={5} />
+          </Grid>
+
+          <PointData
+            data={dataset}
+            x={(d) => d.x}
+            y={(d) => d.y}
+            r={7}
+            fill="${COLOR}"
+          />
         </Cartesian>
       </Chart>
     </svg>
   );
 }
-
-const ChartGrid = () => (
-  <Grid>
-    <Grid.XLines stroke="grey" />
-    <Grid.YLines stroke="grey" />
-    <Grid.XAxes stroke="black" strokeWidth={2} />
-    <Grid.YAxes stroke="black" strokeWidth={2} />
-    <Grid.XLabels padding={5} />
-    <Grid.YLabels padding={5} />
-  </Grid>
-)
-              
 `,
       },
       'data.ts': dataTs,
@@ -334,16 +462,6 @@ export const ExampleLineData = () => (
 import * as d3 from "d3";
 import { dataset } from "./data";
 
-const Data = () => (
-  <LineData
-    data={d3.sort(dataset, (d) => d.x)}
-    x={(d) => d.x}
-    y={(d) => d.y}
-    strokeWidth={2}
-    stroke="${COLOR}"
-  />
-);
-
 export default function App() {
   const width = 400;
   const height = 400;
@@ -363,25 +481,27 @@ export default function App() {
           y={{ scale: "linear", domain: yDomain }}
           nice={true}
         >
-          <ChartGrid />
-          <Data />
+          <Grid>
+            <Grid.XLines stroke="grey" />
+            <Grid.YLines stroke="grey" />
+            <Grid.XAxes stroke="black" strokeWidth={2} />
+            <Grid.YAxes stroke="black" strokeWidth={2} />
+            <Grid.XLabels padding={5} />
+            <Grid.YLabels padding={5} />
+          </Grid>
+
+          <LineData
+            data={d3.sort(dataset, (d) => d.x)}
+            x={(d) => d.x}
+            y={(d) => d.y}
+            strokeWidth={2}
+            stroke="#6bc2be"
+          />
         </Cartesian>
       </Chart>
     </svg>
   );
 }
-
-const ChartGrid = () => (
-  <Grid>
-    <Grid.XLines stroke="grey" />
-    <Grid.YLines stroke="grey" />
-    <Grid.XAxes stroke="black" strokeWidth={2} />
-    <Grid.YAxes stroke="black" strokeWidth={2} />
-    <Grid.XLabels padding={5} />
-    <Grid.YLabels padding={5} />
-  </Grid>
-)
-              
 `,
       },
       'data.ts': dataTs,
@@ -399,15 +519,6 @@ export const ExampleAreaData = () => (
 import * as d3 from "d3";
 import { dataset } from "./data";
 
-const Data = () => (
-  <AreaData
-    data={d3.sort(dataset, (d) => d.x)}
-    x={(d) => d.x}
-    y={(d) => d.y}
-    fill="${COLOR}"
-  />
-);
-
 export default function App() {
   const width = 400;
   const height = 400;
@@ -427,25 +538,26 @@ export default function App() {
           y={{ scale: "linear", domain: yDomain }}
           nice={true}
         >
-          <ChartGrid />
-          <Data />
+          <Grid>
+            <Grid.XLines stroke="grey" />
+            <Grid.YLines stroke="grey" />
+            <Grid.XAxes stroke="black" strokeWidth={2} />
+            <Grid.YAxes stroke="black" strokeWidth={2} />
+            <Grid.XLabels padding={5} />
+            <Grid.YLabels padding={5} />
+          </Grid>
+
+          <AreaData
+            data={d3.sort(dataset, (d) => d.x)}
+            x={(d) => d.x}
+            y={(d) => d.y}
+            fill="#6bc2be"
+          />
         </Cartesian>
       </Chart>
     </svg>
   );
 }
-
-const ChartGrid = () => (
-  <Grid>
-    <Grid.XLines stroke="grey" />
-    <Grid.YLines stroke="grey" />
-    <Grid.XAxes stroke="black" strokeWidth={2} />
-    <Grid.YAxes stroke="black" strokeWidth={2} />
-    <Grid.XLabels padding={5} />
-    <Grid.YLabels padding={5} />
-  </Grid>
-)
-              
 `,
       },
       'data.ts': dataTs,
@@ -463,18 +575,6 @@ export const ExampleRectData = () => (
 import * as d3 from "d3";
 import { dataset } from "./data";
 
-const Data = () => (
-  <RectData
-    data={dataset}
-    x={(d) => d.x}
-    y={(d) => d.y}
-    width={20}
-    height={20}
-    rx={6}
-    fill="${COLOR}"
-  />
-);
-
 export default function App() {
   const width = 400;
   const height = 400;
@@ -494,25 +594,29 @@ export default function App() {
           y={{ scale: "linear", domain: yDomain }}
           nice={true}
         >
-          <ChartGrid />
-          <Data />
+          <Grid>
+            <Grid.XLines stroke="grey" />
+            <Grid.YLines stroke="grey" />
+            <Grid.XAxes stroke="black" strokeWidth={2} />
+            <Grid.YAxes stroke="black" strokeWidth={2} />
+            <Grid.XLabels padding={5} />
+            <Grid.YLabels padding={5} />
+          </Grid>
+
+          <RectData
+            data={dataset}
+            x={(d) => d.x}
+            y={(d) => d.y}
+            width={20}
+            height={20}
+            rx={6}
+            fill="#6bc2be"
+          />
         </Cartesian>
       </Chart>
     </svg>
   );
 }
-
-const ChartGrid = () => (
-  <Grid>
-    <Grid.XLines stroke="grey" />
-    <Grid.YLines stroke="grey" />
-    <Grid.XAxes stroke="black" strokeWidth={2} />
-    <Grid.YAxes stroke="black" strokeWidth={2} />
-    <Grid.XLabels padding={5} />
-    <Grid.YLabels padding={5} />
-  </Grid>
-)
-              
 `,
       },
       'data.ts': dataTs,
@@ -529,15 +633,6 @@ export const ExampleBarData = () => (
         code: `import { Chart, Cartesian, Grid, BarData } from "react-composable-charts";
 import * as d3 from "d3";
 import { dataset } from "./data";
-
-const Data = () => (
-  <BarData
-    data={dataset}
-    x={(d) => d.key}
-    y={{ to: (d) => d.y }}
-    fill="${COLOR}"
-  />
-);
 
 export default function App() {
   const width = 400;
@@ -558,25 +653,26 @@ export default function App() {
           y={{ scale: "linear", domain: yDomain }}
           nice={true}
         >
-          <ChartGrid />
-          <Data />
+          <Grid>
+            <Grid.XLines stroke="grey" />
+            <Grid.YLines stroke="grey" />
+            <Grid.XAxes stroke="black" strokeWidth={2} />
+            <Grid.YAxes stroke="black" strokeWidth={2} />
+            <Grid.XLabels padding={5} />
+            <Grid.YLabels padding={5} />
+          </Grid>
+
+          <BarData
+            data={dataset}
+            x={(d) => d.key}
+            y={{ to: (d) => d.y }}
+            fill="#6bc2be"
+          />
         </Cartesian>
       </Chart>
     </svg>
   );
 }
-
-const ChartGrid = () => (
-  <Grid>
-    <Grid.XLines stroke="grey" />
-    <Grid.YLines stroke="grey" />
-    <Grid.XAxes stroke="black" strokeWidth={2} />
-    <Grid.YAxes stroke="black" strokeWidth={2} />
-    <Grid.XLabels padding={5} />
-    <Grid.YLabels padding={5} />
-  </Grid>
-)
-              
 `,
       },
       'data.ts': dataTs,
@@ -593,26 +689,6 @@ export const ExampleLabelData = () => (
         code: `import { Chart, Cartesian, Grid, PointData, LabelsData } from "react-composable-charts";
 import * as d3 from "d3";
 import { dataset } from "./data";
-
-const Data = () => (
-  <>
-    <LabelsData
-      data={dataset}
-      dataX={(d) => d.x}
-      dataY={(d) => d.y}
-      textAnchor="middle"
-      offsetY={-10}
-      text={(d) => d.key}
-    />
-    <PointData
-      data={dataset}
-      x={(d) => d.x}
-      y={(d) => d.y}
-      r={2}
-      fill="${COLOR}"   
-    />
-  </>
-);
 
 export default function App() {
   const width = 400;
@@ -633,25 +709,36 @@ export default function App() {
           y={{ scale: "linear", domain: yDomain }}
           nice={true}
         >
-          <ChartGrid />
-          <Data />
+          <Grid>
+            <Grid.XLines stroke="grey" />
+            <Grid.YLines stroke="grey" />
+            <Grid.XAxes stroke="black" strokeWidth={2} />
+            <Grid.YAxes stroke="black" strokeWidth={2} />
+            <Grid.XLabels padding={5} />
+            <Grid.YLabels padding={5} />
+          </Grid>
+
+          <LabelsData
+            data={dataset}
+            dataX={(d) => d.x}
+            dataY={(d) => d.y}
+            textAnchor="middle"
+            offsetY={-10}
+            text={(d) => d.key}
+          />
+
+          <PointData
+            data={dataset}
+            x={(d) => d.x}
+            y={(d) => d.y}
+            r={2}
+            fill="#6bc2be"
+          />
         </Cartesian>
       </Chart>
     </svg>
   );
 }
-
-const ChartGrid = () => (
-  <Grid>
-    <Grid.XLines stroke="grey" />
-    <Grid.YLines stroke="grey" />
-    <Grid.XAxes stroke="black" strokeWidth={2} />
-    <Grid.YAxes stroke="black" strokeWidth={2} />
-    <Grid.XLabels padding={5} />
-    <Grid.YLabels padding={5} />
-  </Grid>
-)
-              
 `,
       },
       'data.ts': dataTs,
@@ -668,14 +755,6 @@ export const ExampleLineFunction = () => (
       '/App.tsx': {
         code: `import { Chart, Cartesian, Grid, LineFunction } from "react-composable-charts";
 
-const Data = () => (
-  <LineFunction
-    fn={(x) => Math.sin(x * Math.PI * 2) * 0.5}
-    stroke="${COLOR}"
-    strokeWidth={2}
-  />
-);
-
 export default function App() {
   const width = 400;
   const height = 400;
@@ -692,26 +771,25 @@ export default function App() {
           x={{ scale: "linear", domain: [-1, 1] }}
           y={{ scale: "linear", domain: [-1, 1] }}
         >
-          <ChartGrid />
-          <Data />
+          <Grid>
+            <Grid.XLines stroke="grey" />
+            <Grid.YLines stroke="grey" />
+            <Grid.XAxes stroke="black" strokeWidth={2} />
+            <Grid.YAxes stroke="black" strokeWidth={2} />
+            <Grid.XLabels padding={5} />
+            <Grid.YLabels padding={5} />
+          </Grid>
+
+          <LineFunction
+            fn={(x) => Math.sin(x * Math.PI * 2) * 0.5}
+            stroke="#6bc2be"
+            strokeWidth={2}
+          />
         </Cartesian>
       </Chart>
     </svg>
   );
 }
-
-const ChartGrid = () => (
-  <Grid>
-    <Grid.XLines stroke="grey" />
-    <Grid.YLines stroke="grey" />
-    <Grid.XAxes stroke="black" strokeWidth={2} />
-    <Grid.YAxes stroke="black" strokeWidth={2} />
-    <Grid.XLabels padding={5} />
-    <Grid.YLabels padding={5} />
-  </Grid>
-);
-
-              
 `,
       },
     }}
@@ -727,15 +805,6 @@ export const ExampleAreaFunction = () => (
       '/App.tsx': {
         code: `import { Chart, Cartesian, Grid, AreaFunction } from "react-composable-charts";
 
-const Data = () => (
-  <AreaFunction
-    fn={(x) => Math.sin(x * Math.PI * 2) * 0.5 + 0.25}
-    fnBase={(x) => Math.sin(x * Math.PI * 2) * 0.5 - 0.25}
-    fill="${COLOR}"
-    opacity={0.5}
-  />
-);
-
 export default function App() {
   const width = 400;
   const height = 400;
@@ -752,25 +821,26 @@ export default function App() {
           x={{ scale: "linear", domain: [-1, 1] }}
           y={{ scale: "linear", domain: [-1, 1] }}
         >
-          <ChartGrid />
-          <Data />
+          <Grid>
+            <Grid.XLines stroke="grey" />
+            <Grid.YLines stroke="grey" />
+            <Grid.XAxes stroke="black" strokeWidth={2} />
+            <Grid.YAxes stroke="black" strokeWidth={2} />
+            <Grid.XLabels padding={5} />
+            <Grid.YLabels padding={5} />
+          </Grid>
+
+          <AreaFunction
+            fn={(x) => Math.sin(x * Math.PI * 2) * 0.5 + 0.25}
+            fnBase={(x) => Math.sin(x * Math.PI * 2) * 0.5 - 0.25}
+            fill="#6bc2be"
+            opacity={0.5}
+          />
         </Cartesian>
       </Chart>
     </svg>
   );
 }
-
-const ChartGrid = () => (
-  <Grid>
-    <Grid.XLines stroke="grey" />
-    <Grid.YLines stroke="grey" />
-    <Grid.XAxes stroke="black" strokeWidth={2} />
-    <Grid.YAxes stroke="black" strokeWidth={2} />
-    <Grid.XLabels padding={5} />
-    <Grid.YLabels padding={5} />
-  </Grid>
-);
-              
 `,
       },
     }}
--- documentation/src/Code.tsx
+++ documentation/src/Code.tsx
@@ -4,7 +4,7 @@ export const Code = ({ children }: { children: ReactNode }) => (
   <code
     style={{
       background: '#eceef0',
-      padding: '0.2rem 0.4rem',
+      padding: '0.1rem 0.3rem',
       borderRadius: '3px',
     }}
   >
